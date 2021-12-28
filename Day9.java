c2902067cfe13a62b9b3476a9460336e8aa23492
dropberry <95830709+dropberry@users.noreply.github.com>
2021-12-28 17:48:34 +0100

    Update and rename aoc9.java to Day9.java

--- Day9.java
+++ Day9.java
@@ -1,9 +1,6 @@
 package adventofcode21;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
